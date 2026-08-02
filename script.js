24bb4d0ab7d5234015c03d2dd85380733a6f1ec0
snige40 <snige23@gmail.com>
2026-08-02 22:45:08 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -2,3 +2,54 @@ function showMusic() {
     document.getElementById("welcome").style.display = "none";
     document.getElementById("music").style.display = "block";
 }
+const tracks = [
+    "ncs1.mp3",
+    "ncs2.mp3",
+    "ncs3.mp3",
+    "ncs4.mp3",
+    "ncs5.mp3",
+    "ncs6.mp3",
+    "ncs7.mp3",
+    "ncs8.mp3"
+];
+
+const player = document.getElementById("player");
+const playBtn = document.getElementById("playPauseBtn");
+
+let started = false;
+
+function playRandom() {
+    const random = Math.floor(Math.random() * tracks.length);
+    player.src = tracks[random];
+    player.play();
+}
+
+playBtn.onclick = () => {
+    if (!started) {
+        playRandom();
+        started = true;
+        playBtn.textContent = "⏸";
+        return;
+    }
+
+    if (player.paused) {
+        player.play();
+        playBtn.textContent = "⏸";
+    } else {
+        player.pause();
+        playBtn.textContent = "▶";
+    }
+};
+
+player.onended = () => {
+    playRandom();
+};
+document.getElementById("nextBtn").onclick = playRandom;
+document.getElementById("prevBtn").onclick = playRandom;
+document.getElementById("nextBtn").onclick = () => {
+    playRandom();
+};
+
+document.getElementById("prevBtn").onclick = () => {
+    playRandom();
+};
